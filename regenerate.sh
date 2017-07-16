2790e5d79dd804579a7ebe1c84a1bb5a51d26d00
Pierre <pierre@hubert.fr>
2017-07-16 16:20:01 +0200

    Added alt name

--- regenerate.sh
+++ regenerate.sh
@@ -15,11 +15,17 @@ for name in $(cat $domainsList) ; do
 	#Inform user about what's happening
 	echo "Generate certificate for domain: " $name;
 
+	#Create temporary openssl configuration file
+	cp openssl.cnf tmp/openssl.tmp.cnf
+
+	#Complete temporary OpenSSL configuration file
+	echo "DNS.1 = "`echo $name` >> tmp/openssl.tmp.cnf
+
 	#Generate private key + CSR file
-	openssl req -new -nodes -out tmp/certreq.pem -keyout tmp/privkeyreq.pem -config ./openssl.cnf -subj /CN=$name
+	openssl req -new -nodes -out tmp/certreq.pem -keyout tmp/privkeyreq.pem -config ./tmp/openssl.tmp.cnf -subj /CN=$name
 
 	#Sign certificate
-	openssl ca -batch -extensions v3_req -outdir ./tmp/ -out ./tmp/cert.pem  -config ./openssl.cnf -cert `echo $caCertFolder`ca-public.pem -keyfile `echo $caCertFolder`ca-private.pem -infiles tmp/certreq.pem
+	openssl ca -batch -extensions v3_req -outdir ./tmp/ -out ./tmp/cert.pem  -config ./tmp/openssl.tmp.cnf -cert `echo $caCertFolder`ca-public.pem -keyfile `echo $caCertFolder`ca-private.pem -infiles tmp/certreq.pem
 
 	#Move newly generated certificate
 	targetDir=`echo $destination``echo $name`
@@ -31,8 +37,14 @@ for name in $(cat $domainsList) ; do
 
 	#Empty temporary directory
 	rm `echo $opensslConfigDir`tmp/*.pem
+
+	#Remove temporary configuration
+	rm tmp/openssl.tmp.cnf
 done;
 
 #Empty database
 rm `echo $opensslConfigDir`index.tx*
 touch `echo $opensslConfigDir`index.txt
+
+#Done message
+echo "Certificates successfully generated !"
